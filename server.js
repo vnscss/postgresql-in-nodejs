646ed0bb8fd3f3d0cb4f01811d2c47f007b90412
Vinícius <viniccius.alves.pires@gmail.com>
2024-10-20 20:47:11 -0300

    EJS configuration

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const cors = require('cors')
 const db = require('./dataBase/dataBase.js');
 
 const app = express();
 
-app.get('/', async (req, res) => {
+
+app.use(cors());
+
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+
+app.set('views', './views', 'views');
+app.use(express.static('./static'));
+
+app.locals.baseURL = "https://localhost:1337"
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/sql', async (req, res) => {
+  let data = await req.body
+  console.log(req.body)
   try {
-    const result = await db.query('SELECT * FROM users;');
+    const result = await db.query(data.query);
     res.json(result.rows);
   } catch (err) {
     console.error(err);
@@ -13,6 +32,6 @@ app.get('/', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(1337, () => {
   console.log('Server is running on port 1337');
 });
