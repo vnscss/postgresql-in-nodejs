da8e4d09fa21a74fdb43a05dea02eb3623d7d79c
Vinícius <viniccius.alves.pires@gmail.com>
2024-10-20 22:32:51 -0300

    Ajustes no frontend

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const db = require('./dataBase/dataBase.js');
 
 const app = express();
 
-
 app.use(cors());
 
 app.use(express.json());
@@ -14,17 +13,48 @@ app.set('view engine', 'ejs');
 app.set('views', './views', 'views');
 app.use(express.static('./static'));
 
-app.locals.baseURL = "https://localhost:1337"
+app.locals.baseURLHTTPS = "https://localhost:1337"
+app.locals.baseURL = "http://localhost:1337"
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+
 app.post('/sql', async (req, res) => {
   let data = await req.body
-  console.log(req.body)
   try {
     const result = await db.query(data.query);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/get-users', async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM users;");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/get-equipamento', async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM equipamento;");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/:query', async (req, res) => {
+  try {
+    const result = await db.query(req.params.query);
     res.json(result.rows);
   } catch (err) {
     console.error(err);
